9a127208103e71a7d8085db342aa10cfeb3f7249
Siddhartha Tyagi <tyagisiddhartha156@gmail.com>
2026-09-08 09:00:03 +0530

    Prepare app for deployment

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,7 @@
 import { getActiveUser, getBackendUserId } from "./userState";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
 
 async function apiFetch(
   url: string,
